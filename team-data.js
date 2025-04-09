9cb22a6fa88056cba19c4dc87cdfc8ed88c89494
bolivarc3 <carlsbolivar@gmail.com>
2025-04-08 19:03:06 -0500

    Intial Homepage, added some images

---
+++ team-data.js
@@ -0,0 +1,38 @@
+// Team Member Data
+const teamMembers = [
+    {
+        id: 1,
+        name: "Mark Chambers",
+        title: "President/CEO",
+        image: "member1.jpg",
+        bio: ""
+    },
+    {
+        id: 2,
+        name: "David Thrasher",
+        title: "Financial Consultant Emeritus",
+        image: "member2.jpg", 
+        bio: ""
+    },
+    {
+        id: 3,
+        name: "Trey Taylor",
+        title: "Financial Consultant",
+        image: "member3.jpg",
+        bio: ""
+    },
+    {
+        id: 4,
+        name: "Kim Ruopp",
+        title: "Office Manager/Assistant",
+        image: "member4.jpg",
+        bio: ""
+    },
+    {
+        id: 5,
+        name: "Mary Presley",
+        title: "Assistant",
+        image: "member5.jpg",
+        bio: ""
+    }
+];
