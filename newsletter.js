5fecb901197f0e41784a0f261cee7790fe4f9bdf
Avinash Devineni <devinenia69420@gmail.com>
2025-04-26 14:56:58 -0500

    Changed newsletter styling and removed description

--- newsletter.js
+++ newsletter.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     // will fetch from DB later
     const dummyData = [
-        {title: 'This Is An Example', date: '4/22/2025', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quidem ratione sit consequatur adipisci esse suscipit modi pariatur obcaecati commodi perferendis officiis est ipsa, dicta, temporibus nisi sed id deleniti.', imgUrl: null},
-        {title: 'This Is An Example', date: '4/22/2025', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quidem ratione sit consequatur adipisci esse suscipit modi pariatur obcaecati', imgUrl: 'public/road.jpg'},
-        {title: 'Another Example', date: '4/22/2025', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quidem ratione sit consequatur adipisci esse suscipit modi pariatur obcaecati commodi perferendis officiis est ipsa, dicta, temporibus nisi sed id deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.', imgUrl: 'public/road.jpg'},
+        {title: 'How to navigate the new financial landscape amidst tumulus times', date: '4/22/2025', imgUrl: 'public/road.jpg'},
+        {title: 'How to navigate the new financial landscape amidst tumulus times', date: '4/22/2025', imgUrl: 'public/road.jpg'},
+        {title: 'How to navigate the new financial landscape amidst tumulus times', date: '4/22/2025', imgUrl: 'public/road.jpg'},
+        {title: 'How to navigate the new financial landscape amidst tumulus times', date: '4/22/2025', imgUrl: 'public/road.jpg'},
+        {title: 'How to navigate the new financial landscape amidst tumulus times', date: '4/22/2025', imgUrl: 'public/road.jpg'},
+        {title: 'How to navigate the new financial landscape amidst tumulus times', date: '4/22/2025', imgUrl: 'public/road.jpg'},
+        {title: 'Another Example', date: '4/22/2025', imgUrl: 'public/road.jpg'},
     ]
 
-    const newsletters = document.getElementById('newsletters-list');
+    const newsletters = document.getElementById('newsletters-container');
 
     dummyData.forEach(data => {
         const newsletter = document.createElement('div');
@@ -14,27 +18,24 @@ document.addEventListener('DOMContentLoaded', () => {
         // if (data.title === 'Another Example')
         //     newsletter.className = 'newsletter-v2';
 
-        const title = document.createElement('h2');
+        const title = document.createElement('p');
+        title.className = 'newsletter-title';
         title.textContent = data.title;
 
         const date = document.createElement('p');
+        date.className = 'newsletter-date';
         date.textContent = data.date;
-        
-        const description = document.createElement('p')
-        description.textContent = data.description;
         
         const info = document.createElement('div');
         info.className = 'newsletter-info';
         info.appendChild(title);
         info.appendChild(date);
-        info.appendChild(description);
 
-        if (data.imgUrl != null) {
-            const img = document.createElement('img');
-            img.src = data.imgUrl;
-            newsletter.appendChild(img);
-        }
-
+        const img = document.createElement('div');
+        img.className = 'newsletter-img';
+        img.style.background = `url(${data.imgUrl})`;
+        
+        newsletter.appendChild(img);
         newsletter.appendChild(info);
         newsletters.appendChild(newsletter);
     });
